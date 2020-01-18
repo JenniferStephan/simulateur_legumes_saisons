eca0874db652e01fb40e85753d5b5c97885c0957
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-01-18 16:07:07 +0100

    view du simulateur avec les radio buttons ok

---
+++ app/controllers/simulations_controller.rb
@@ -0,0 +1,41 @@
+class SimulationsController < ApplicationController
+
+before_action :set_simulation, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @simulation = Simulation.new
+  end
+
+  def create
+    @simulation = Simulation.new(simulation_params)
+
+    @simulation.save
+
+  end
+
+  def index
+    @simulations = Simulation.all
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_simulation
+    @simulation = Simulation.find(params[:id])
+  end
+
+  def simulation_params
+    params.require(:simulation).permit(:ask_month, :ask_vege, :answer)
+  end
+end
--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,2 +1,39 @@
 class Simulation < ApplicationRecord
+before_save :set_answer
+
+FL = ["Ail", "Artichaut", "Asperge", "Aubergine",
+    "Betterave", "Blette", "Brocoli", "Carotte", "Céleri",
+    "ChampignondeParis", "Chou", "ChoudeBruxelles",
+    "Chou-fleur", "Concombre", "Courge", "Courgette", "Cresson",
+    "Échalote","Endive", "Épinard", "Fenouil", "Haricotvert",
+    "Laitue", "Mâche", "Navet",
+    "Maïs", "Oignon", "Panais", "Petitpois", "Poireau",
+    "Poivron","Potiron", "Radis", "Salsifis", "Tomate",
+    "Topinambour", "Abricot", "Cassis", "Cerise", "Citron",
+    "Châtaigne","Clémentine", "Coing", "Figue", "Fraise",
+    "Framboise", "Groseille", "Kiwi",
+    "Mandarine", "Melon", "Mirabelle", "Mûre", "Myrtille",
+    "Nectarine","Noisette", "Noix", "Orange", "Pamplemousse",
+    "Pêche", "Poire", "Pomme",
+    "Prune", "Quetsche", "Raisin", "ReineClaude", "Rhubarbe"]
+  MONTH = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
+
+  def set_result
+    # if activity == "Libérale" && year_existence == 1 && reglementary == false && accre == true
+    #   self.result = 0.06
+    #   self.user.update(tax_rate: self.result)
+    # elsif activity == "Libérale" && year_existence == 2 && reglementary == false && accre == true
+    #   self.result = 0.12
+    #   self.user.update(tax_rate: self.result)
+    # elsif activity == "Libérale" && year_existence == 3 && reglementary == false && accre == true
+    #   self.result = 0.17
+    #   self.user.update(tax_rate: self.result)
+    # elsif activity == "Libérale" && year_existence == 4 && reglementary == false && accre == true
+    #   self.result = 0.23
+    #   self.user.update(tax_rate: self.result)
+    # elsif activity == "Libérale" && accre == false
+    #   self.result = 0.23
+    #   self.user.update(tax_rate: self.result)
+    # end
+  end
 end
