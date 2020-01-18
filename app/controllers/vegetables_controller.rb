85a277258f4475900620a3d4afc9084ab4a8319f
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-01-18 15:29:09 +0100

    created controller

---
+++ app/controllers/vegetables_controller.rb
@@ -0,0 +1,27 @@
+class VegetablesController < ApplicationController
+before_action :set_vege, only: [:show, :edit, :update]
+
+  def new
+    @vege = Vegetable.new
+  end
+
+  def create
+  end
+
+  def index
+    @veges = Vegetable.all
+  end
+
+  private
+
+private
+
+  def set_vege
+   @vege = Vegetable.find(params[:id])
+  end
+
+  def veges_params
+    params.require(:vegetable).permit(:label, :months)
+  end
+
+end
---
+++ db/migrate/20200118142522_create_vegetables.rb
@@ -0,0 +1,11 @@
+class CreateVegetables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :vegetables do |t|
+      t.string 'label'
+      t.string 'months', array: true
+
+
+      t.timestamps
+    end
+  end
+end
